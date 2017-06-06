75d20af01c5b66915402a589202b7b6b5a60fe0f
Carl Jahn <carl.jahn@telekom.de>
2017-06-06 23:07:52 +0200

    implemented learning curve

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -54,8 +54,17 @@ error_val   = zeros(m, 1);
 % ---------------------- Sample Solution ----------------------
 
 
+for i = 1:m
 
+  theta = trainLinearReg(X(1:i, :), y(1:i), lambda);
 
+  [trainError, grad] = linearRegCostFunction(X(1:i, :), y(1:i), theta, 0);
+  [cvError, grad] = linearRegCostFunction(Xval, yval, theta, 0);
+
+  error_train(i) = trainError;
+  error_val(i) = cvError;
+
+end
 
 
 
