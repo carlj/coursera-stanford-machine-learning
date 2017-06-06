bd4f7eb57d583ce470c890684b5404ed5b9f3aad
Carl Jahn <carl.jahn@telekom.de>
2017-06-06 22:47:54 +0200

    implemented poly features exercise

--- machine-learning-ex5/ex5/polyFeatures.m
+++ machine-learning-ex5/ex5/polyFeatures.m
@@ -10,15 +10,15 @@ function [X_poly] = polyFeatures(X, p)
 X_poly = zeros(numel(X), p);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Given a vector X, return a matrix X_poly where the p-th 
+% Instructions: Given a vector X, return a matrix X_poly where the p-th
 %               column of X contains the values of X to the p-th power.
 %
-% 
+%
 
+power_vector = [1:p];
+X_repmat_matrix = repmat(X, 1, p);
 
-
-
-
+X_poly = X_repmat_matrix .^ power_vector;
 
 % =========================================================================
 
