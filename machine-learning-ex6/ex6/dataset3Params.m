f8e52fb40f6ba587f5beef0ad5a5dd937139c6e4
Carl Jahn <carl.jahn@telekom.de>
2017-06-16 21:07:45 +0200

    implemented dataset3Params

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,18 +15,38 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+values = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+numberOfValues = length(values);
 
+predictError = intmax;
 
+for i = 1:numberOfValues^2
 
+  currentC = values(ceil(i / numberOfValues));
+  currentSigma = values(mod(i, numberOfValues) + 1);
 
+  fprintf(['C = %f Sigma = %f \n'], currentC, currentSigma);
+
+  model = svmTrain(X, y, currentC, @(x1, x2) gaussianKernel(x1, x2, currentSigma));
+  predictions = svmPredict(model, Xval);
+
+  currentPredictError = mean(double(predictions ~= yval));
+
+  if currentPredictError < predictError
+    C = currentC;
+    sigma = currentSigma;
+    predictError = currentPredictError;
+  end
+
+end
 
 
 % =========================================================================
